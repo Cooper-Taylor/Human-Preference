9501786ba9c17d40c120412a7f4a6972e5d3983e
Cooper Taylor <cooperjtaylor2005@gmail.com>
2024-06-17 12:31:40 -0400

    Updated label extract and implemented data upload feature

--- full_pipeline.py
+++ full_pipeline.py
@@ -1,8 +1,8 @@
 # Imports
 import argparse
 import os
-import json
-import pandas
+import stat
+import sys
 
 # Command line arguments
 def parse_args():
@@ -10,7 +10,6 @@ def parse_args():
     parser.add_argument("output_dir", help="Path to output directory where labels will be stored.")
     parser.add_argument("input_path", help="Path to the input CSV file.")
     parser.add_argument("extraction_projectID", help="ProjectID to access and extract information.")
-    parser.add_argument("--uploading_project_name", help="Project name to upload LLM and human-labeled frames")
     parser.add_argument("--labelbox_api_key_loc", default=os.path.expanduser('~/.apikeys/labelbox_api_key.txt'), help="Location of text file containing LabelBox API key.")
     
     parser.add_argument("--extract_data", action='store_true')
@@ -21,10 +20,12 @@ def parse_args():
     parser.add_argument("--results_dir", default=os.path.join('.', 'output'), help="Directory where results (and intermediate temp file) will be written.")
     parser.add_argument("--labeled_data_path", default=os.path.join('.', 'data', 'labeled_data.csv'), help="Path for labeled data file.")
     parser.add_argument("--text_col", default='text', help="Name of the text column.")
-    parser.add_argument("--openai_api_key_loc", default=os.path.join('.', 'openai_api_key.txt'), help="Location of text file containing OpenAI API key.")
+    parser.add_argument("--openai_api_key_loc", default=os.path.expanduser('~/.apikeys/openai_api_key.txt'), help="Location of text file containing OpenAI API key.")
     parser.add_argument("--raw_csv_or_intermediate", default='c', help="Whether to use the input_path data file (c) or an intermediate file (i). Default (c). Only use (i) if previous frame extraction was interrupted before completion.")
     parser.add_argument("--system_prompt_loc", default=os.path.join('.', 'utils', 'oai_system_message_template.txt'), help="System prompt to give to LLM when extracting frames.")
+   
     parser.add_argument("--upload_data", action='store_true')
+    parser.add_argument("--labeled_data", default=os.path.join('.', 'data', 'frame_extraction_results.csv'), help="Data path of data to be uploaded to labelbox")
 
     return parser.parse_args()
 
@@ -42,7 +43,8 @@ def full_pipeline(output_dir,
                   openai_api_key_loc,
                   raw_csv_or_intermediate,
                   system_prompt_loc,
-                  upload_data):
+                  upload_data,
+                  labeled_data):
     
     if extract_data:
         print("Extracting Data...")
@@ -54,6 +56,17 @@ def full_pipeline(output_dir,
         
     if append_LLM_labels:
         print("Append AI Labels")
+        sys.path.append(r'C:\Users\coope\InternshipCode\social_media_frame_analysis')
+
+        directory = 'C:\\Users\\coope\\InternshipCode\\human_preference\\data'
+
+        # Check current permissions
+        permissions = os.stat(directory).st_mode
+        print(oct(permissions))
+
+        # Change permissions
+        os.chmod(directory, permissions | stat.S_IRWXU)
+
         from frame_extraction.extract_frames import process_and_save_posts
         process_and_save_posts(input_path,
                            results_dir,
@@ -62,6 +75,12 @@ def full_pipeline(output_dir,
                            openai_api_key_loc,
                            raw_csv_or_intermediate,
                            system_prompt_loc)
+        
+    if upload_data:
+        print("Uploading Data to LabelBox")
+        from upload_labels import upload_dataset
+        upload_dataset(labelbox_api_key_loc, 
+                       labeled_data)
 
 
 if __name__ == "__main__":
@@ -79,4 +98,5 @@ if __name__ == "__main__":
                 args.openai_api_key_loc,
                 args.raw_csv_or_intermediate,
                 args.system_prompt_loc,
-                args.upload_data)
+                args.upload_data,
+                args.labeled_data)
--- upload_labels.py
+++ upload_labels.py
@@ -1,13 +1,30 @@
 import labelbox as lb
 import pandas as pd
 import os
+from random import shuffle
+import string
+import ast
 
 '''
 Use this function to upload the data with appended LLM responses to labelbox using conversation text data row formats.
 Link to Doc: https://docs.labelbox.com/reference/import-conversation-model-response-data
 '''
 
-def upload_data(projectID, api_key_loc, data_path):
+import string
+
+def normalize_string(s):
+    # Remove leading and trailing whitespace
+    s = s.strip()
+    # Capitalize the first letter and make other letters lowercase
+    s = s.capitalize()
+    # Ensure there's a period at the end if there's no punctuation
+    if (s != "None"):
+        if s and s[-1] not in string.punctuation:
+            s += '.'
+
+    return s
+
+def upload_dataset(api_key_loc, data_path):
     # Find api key using api_key_loc if path exists
     if not os.path.exists(api_key_loc):
         error_message = f'API key not found at {api_key_loc}'
@@ -33,17 +50,57 @@ def upload_data(projectID, api_key_loc, data_path):
     # Will be list of datarow objects with global key
     data_rows = []
     
-    # Iterate through dataframe rows appending 
+    # Iterate through dataframe rows appending objecs to data_rows
     for index, row in df.iterrows():
-        row_data = {"type": "application/vnd.labelbox.conversational",
-                "version": 1,
-                "messages": [
-                    {"messageId" : f"message-{row["global_key"]}",
-                     "content" : row["row_data"],
-                     "user": {
-                         "userId" : f"User {row["global_key"]}"
-                     }}
-                ],
-                "modelOutputs": []
-        }
+        if len(row["label"][0]) != 0:
+            # Unsure if labelbox randomizes order of responses, so shuffle
+            responses = [{"labeler" : f"{row["label_author"]}", "data" : ast.literal_eval(row["label"])},
+                          {"labeler" : "ChatGPT4o", "data" : ast.literal_eval(row["frames"])}]
+            shuffle(responses)
 
+            # Standardize the response strings
+
+            responses[0]["data"] = '\n'.join([normalize_string(s) for s in responses[0]["data"]])
+            responses[1]["data"] = '\n'.join([normalize_string(s) for s in responses[1]["data"]])
+
+
+            row_data = {"type": "application/vnd.labelbox.conversational",
+                    "version": 1,
+                    "messages": [
+                        {"messageId" : f"message-{row["global_key"]}",
+                        "content" : row["row_data"],
+                        "user": {
+                            "userId" : f"User {row["global_key"]}",
+                            "name" : row["post_author"]
+                        },
+                        "align": "left",
+                        "canLabel": False
+                        }
+                    ],
+                    "modelOutputs": [
+                        {
+                            "title" : "Response A",
+                            "content" : responses[0]["data"],
+                            "modelConfigName" : responses[0]["labeler"]
+                        },
+                        {
+                            "title" : "Response B",
+                            "content" : responses[1]["data"],
+                            "modelConfigName": responses[1]["labeler"]
+                        }
+                    ]
+            }
+
+            # Append row_data to data_rows
+            # Because of the nature of the project global keys, add _hp to download them
+            data_rows.append({
+                "row_data" : row_data,
+                "global_key" : str(row["global_key"]) + '_hp'
+            })
+
+
+    task = dataset.create_data_rows(data_rows)
+
+    task.wait_till_done()
+    print("Errors:",task.errors)
+    print("Failed data rows:", task.failed_data_rows)
